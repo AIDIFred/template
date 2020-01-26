e5c727262ed3d4c275b77f1cf2c4a45ae850f891
Dan <danevans264@gmail.com>
2020-01-26 12:24:28 -0600

    test

--- registerbe.php
+++ registerbe.php
@@ -6,13 +6,13 @@ $db = fread($filer);
 fclose($filer);
 
 $hashed = password_hash($_POST['pword'],PASSWORD_BCRYPT);
-
+/*
 $db+="\n";
 $dbfile = fopen("users.mndb",'w');
 fwrite($dbfile,$db+$_POST['name']+'^'+$_POST['email']+'^'+$_POST['uname']+'^'+$hashed);
 fclose($dbfile);
 
-
+*/
 
 ?>
 
