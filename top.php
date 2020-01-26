4e5fa16ff425650b8c0228b7a7a7f22a727d2702
Dan <danevans264@gmail.com>
2020-01-26 09:29:51 -0600

    log buttons

--- top.php
+++ top.php
@@ -15,12 +15,24 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-
+	<?php include "dbconn.php"; ?>
 
 
 </head>
 <body>
 	<div class="trow nav_container">
-		<div class="navcell">
+		<div class="navcell" onclick="window.location.replace('home.php');">
 			Home
 		</div>
+		
+		<div class="navlog trow">
+			<?php 
+				if (!isset($_SESSION['uname'])) {
+					echo "<span class='login-btn' onclick='window.location.replace(\"login.php\")'>Login</span> <span class='register-btn'onclick='window.location.replace(\"register.php\")'>Register</span>";
+				} else {
+					echo "<span class='logout-btn' onclick='window.location.replace(\"logout.php\")'>Logout</span>";
+				}
+			?>
+		</div>
+			
+	</div>
